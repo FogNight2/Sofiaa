f084ae2c5ef9b67730d0aec729997ed49f67e9bf
FogNight2 <165726021+FogNight2@users.noreply.github.com>
2024-04-01 18:31:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const yesBtn = document.querySelector('#yesBtn');
 
 yesBtn.addEventListener('click',function () {
-    alert('Sabia que ibas a decir que si uwu')
+    alert('Yo igual te quiero mucho:(')
 });
 
 const noBtn = document.querySelector('#noBtn');
